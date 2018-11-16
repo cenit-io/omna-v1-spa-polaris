110e0d98fad0a19a8e67e9d9634b6871f2198395
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2018-11-16 09:55:49 -0500

    v1.2.0

--- src/pages/setups/SetupPage.js
+++ src/pages/setups/SetupPage.js
@@ -15,28 +15,39 @@ export class SetupPage extends TabsPage {
         return 'Settings off ' + tab.content + (tab.content === 'OMNA' ? ':' : ' sale channel:')
     }
 
+    isAvailable(channel) {
+        const channels = this.state.appContext.settings.channels || 'all';
+
+        return channels === 'all' || channels.indexOf(channel) != -1
+    }
+
     tabs() {
-        return [
-            {
+        const
+            channels = this.state.appContext.settings.channels || 'all',
+            tabs = [{
                 id: 'setup-omna-tab',
                 content: 'OMNA',
                 body: <SetupOMNA/>
-            },
-            {
-                id: 'setup-lazada-tab',
-                content: 'Lazada',
-                body: <SetupLazada/>
-            },
-            {
-                id: 'setup-qoo10-tab',
-                content: 'Qoo10',
-                body: <SetupQoo10/>
-            },
-            {
-                id: 'setup-shopee-tab',
-                content: 'Shopee',
-                body: <SetupShopee/>
-            }
-        ]
+            }];
+
+        if ( this.isAvailable('Lazada') ) tabs.push({
+            id: 'setup-lazada-tab',
+            content: 'Lazada',
+            body: <SetupLazada/>
+        });
+
+        if ( this.isAvailable('Qoo10') ) tabs.push({
+            id: 'setup-qoo10-tab',
+            content: 'Qoo10',
+            body: <SetupQoo10/>
+        });
+
+        if ( this.isAvailable('Shopee') ) tabs.push({
+            id: 'setup-shopee-tab',
+            content: 'Shopee',
+            body: <SetupShopee/>
+        });
+
+        return tabs
     }
 }
