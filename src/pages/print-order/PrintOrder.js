e662e36fb4cd38e03f92ccd760d2695f6167d23a
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2018-11-26 14:15:45 -0500

    v1.2.1

--- src/pages/print-order/PrintOrder.js
+++ src/pages/print-order/PrintOrder.js
@@ -10,27 +10,36 @@ export class PrintOrder extends OMNAPage {
         this.state.documents = null;
         this.state.loading = true;
         this.state.baseUri = '';
+        this.state.currentDocumentSrc = false;
 
-        this.handleOpenPage = this.handleOpenPage.bind(this);
+        this.handleOpenDocument = this.handleOpenDocument.bind(this);
+        this.printDocument = this.printDocument.bind(this);
         this.loadDocuments = this.loadDocuments.bind(this);
 
         setTimeout(this.loadDocuments, 0);
     }
 
-    handleOpenPage(e) {
+    handleOpenDocument(e) {
         e.preventDefault();
+        this.loadDocument(e.target.href)
+    }
 
+    printDocument(e) {
+        e.target.focus();
+        e.target.contentWindow.print();
+    }
+
+    loadDocument(src) {
         this.loadingOn();
+        console.log(src);
         axios({
-            url: e.target.href,
-            responseType: 'arraybuffer'
+            url: src, responseType: 'arraybuffer'
         }).then((response) => {
             let blob = new Blob([response.data], { type: response.headers['content-type'] }),
                 url = window.URL.createObjectURL(blob),
-                w = open(url, '_blank');
+                iframe = $('.current-print-document');
 
-            w.focus();
-            w.print();
+            iframe.prop('src', url);
         }).catch(
             (error) => this.flashError('Failed to load docuement.' + error)
         ).finally(() => this.loadingOff())
@@ -54,25 +63,51 @@ export class PrintOrder extends OMNAPage {
         }).always(() => this.loadingOff());
     }
 
+    getDocumentUri(doc) {
+        return this.state.baseUri + '&type=' + doc.type
+    }
+
+    renderCurrentDocument() {
+        const { currentDocumentSrc } = this.state;
+
+        if ( !currentDocumentSrc ) return this.renderLoading();
+
+        return <iframe className="current-print-document" onLoad={this.printDocument}></iframe>
+    }
+
     renderDocuments() {
-        const { documents, baseUri } = this.state;
+        const { documents } = this.state;
 
         if ( !documents || documents.length === 0 ) return this.warn('Order print not available...');
 
         return (
-            <FormLayout.Group>
-                {
-                    documents.map((doc, idx) =>
-                        <Banner key={idx} icon="print">
-                            {this.renderExternalLink(doc.title, baseUri + '&type=' + doc.type, this.handleOpenPage)}
-                        </Banner>
-                    )
-                }
-            </FormLayout.Group>
+            <div>
+                <Card.Section>
+                    <FormLayout.Group>
+                        {
+                            documents.map((doc, idx) =>
+                                <Banner key={idx} icon="print">
+                                    {this.renderExternalLink(doc.title, this.getDocumentUri(doc), this.handleOpenDocument)}
+                                </Banner>
+                            )
+                        }
+                    </FormLayout.Group>
+                </Card.Section>
+                <Card.Section>
+                    {this.renderCurrentDocument()}
+                </Card.Section>
+            </div>
         )
     }
 
     renderPageContent() {
         return this.state.loading ? this.renderLoading() : <Card sectioned>{this.renderDocuments()}</Card>
+    }
+
+    componentDidUpdate(prevProps) {
+        const { documents } = this.state;
+        console.log(111, documents);
+
+        if ( documents && documents.length > 0 ) this.loadDocument(this.getDocumentUri(documents[0]))
     }
 }
