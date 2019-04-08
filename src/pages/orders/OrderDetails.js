f55ef83a1b65917ee764f47484f9587888bfa35c
Hansel Bormey <hanselbormey@gmail.com>
2019-04-08 12:09:26 -0400

    Add new information to order detail view
    -Add customer information

--- src/pages/orders/OrderDetails.js
+++ src/pages/orders/OrderDetails.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { OMNAPage } from '../OMNAPage';
-import { DisplayText, Icon, Card, Layout, FormLayout, Stack, Subheading, Badge, TextStyle, Button } from '@shopify/polaris';
-import { Icons  } from '../../common/Icons';
+import { DisplayText, Icon, Card, Layout, Stack, TextStyle, Button } from '@shopify/polaris';
 
 export class OrderDetails extends OMNAPage{
     constructor(props){
@@ -9,28 +8,60 @@ export class OrderDetails extends OMNAPage{
         this.state.title = 'Order Details';
         this.state.subTitle = '';
 
-        this.showShipmentItems = this.showShipmentItems.bind(this);
+        this.showItemsInfo = this.showItemsInfo.bind(this);
+        this.showShipmentsInfo = this.showShipmentsInfo.bind(this);
+        this.printOrder = this.printOrder.bind(this);
+        this.navigateToOrdersPage = this.navigateToOrdersPage.bind(this);
 
     }
 
-    showShipmentItems(order){
-        const line_items_shipment = order.line_items.map(({variant, price, quantity}) =>
-            <Fragment key={variant.sku}>
-                <Stack distribution="fillEvenly">
-                    <div className={"vertical-alignment"}>
-                        <TextStyle variation="positive">{variant.name}</TextStyle>
-                        <TextStyle variation="subdued">{'SKU: '}{variant.sku}</TextStyle>
-                    </div>
 
-                    <p>{'$'}{price}{' x '} {quantity}</p>
-                    <p>{'$'}{price}</p>
-                </Stack>
+    showItemsInfo(order){
+        let line_items_shipment;
+        if (order){
+            line_items_shipment = order.line_items.map(({variant, price, quantity}) =>
+                <Fragment key={variant.sku}>
+                    <Stack distribution="fillEvenly">
+                        <div className={"vertical-alignment"}>
+                            <TextStyle variation="positive">{variant.name}</TextStyle>
+                            <TextStyle variation="subdued">{'SKU: '}{variant.sku}</TextStyle>
+                        </div>
 
+                        <p>{'$'}{price}{' x '} {quantity}</p>
+                        <p>{'$'}{price}</p>
+                    </Stack>
+                </Fragment>
+            );
+        }
+        else {
+            line_items_shipment = null;
+        }
 
-
-            </Fragment>
-        );
         return line_items_shipment;
+    }
+
+    showShipmentsInfo(order) {
+        let shipments;
+
+        if(order) {
+            shipments = order.shipments.map(({tracking})=>
+                <div key={tracking} style={{marginBottom: '5px'}}>
+                    <TextStyle variation="subdued">NinjaVan Dispatch tracking</TextStyle>
+                    <p>{tracking}</p>
+                </div>
+            );
+        }
+
+
+        return shipments;
+    }
+
+    navigateToOrdersPage(){
+        OMNA.render('orders');
+    }
+
+    printOrder(order){
+        OMNA.render('print-order', order);
     }
 
 
@@ -40,21 +71,38 @@ export class OrderDetails extends OMNAPage{
 
         const date = new Date(order.completed_at).toUTCString().split(' ').slice(0, 5).join(' ');
 
-        const line_items_shipment = this.showShipmentItems(order);
+        const  line_items_shipment = this.showItemsInfo(order);
+
+        const shipments = this.showShipmentsInfo(order);
+
+        let icon;
+
+        if ( order.state === 'complete') {
+            icon = <Icon source="checkmark" color="greenDark" backdrop={true}/>;
+        }
+        else{
+            if(order.state === 'canceled'){
+                icon = <Icon source="circleCancel" color="yellowDark" backdrop={true}/>;
+            }
+            else{
+                icon = <Icon source="alert" color="ink" backdrop={true}/>;
+            }
+        }
+
 
         return (
             <div>
                 <div className="header">
                     <Card sectioned>
+                        <div style={{marginBottom: '10px'}}>
+                            <Button icon="arrowLeft" onClick={()=>this.navigateToOrdersPage()} plain>Orders</Button>
+                        </div>
                         <Stack alignment="baseline">
                             <DisplayText size="large">{order.number}</DisplayText>
                             <TextStyle variation="subdued">{date}</TextStyle>
-
                         </Stack>
                         <div style={{marginTop: '10px'}}>
-                                <div style={{color: '#bf0711'}}>
-                                <Button icon="orders" plain>Print Orders</Button>
-                                </div>
+                            <Button icon="orders" onClick={()=>this.printOrder(order)} plain>Print Order</Button>
                         </div>
                     </Card>
 
@@ -67,17 +115,16 @@ export class OrderDetails extends OMNAPage{
                                     <Stack alignment="leading">
                                         <Stack.Item fill>
                                             <div style={{'display': 'flex'}}>
-                                                <Icon source="checkmark" color="green" backdrop={true}/>
+                                                {icon}
                                                 <div style={{marginLeft: '10px'}}>
                                                     <DisplayText size="large">{order.state}</DisplayText>
                                                 </div>
-
                                             </div>
                                             <br/>
-                                            <TextStyle variation="subdued">NinjaVan Dispatch tracking</TextStyle><br/>
-                                            <p>454654564ER98AZ</p>
+                                            <Fragment>
+                                                {shipments}
+                                            </Fragment>
                                         </Stack.Item>
-
                                         <Stack.Item>
                                             {order.channel}
                                         </Stack.Item>
@@ -95,21 +142,17 @@ export class OrderDetails extends OMNAPage{
                                 <Card.Section title="Customer">
                                     <p>{order.customer.firstname}</p>
                                 </Card.Section>
-                                    <Card.Section title="Shipping Address">
-                                        <p>{order.ship_address.firstname}</p>
-                                        <p>{order.ship_address.address1}</p>
-                                        <p>{order.ship_address.city}{' '} {order.ship_address.zipcode}</p>
-                                        <p>{order.ship_address.country.name}</p>
-                                    </Card.Section>
+                                <Card.Section title="Shipping Address">
+                                    <p>{order.ship_address.firstname}</p>
+                                    <p>{order.ship_address.address1}</p>
+                                    <p>{order.ship_address.city}{' '} {order.ship_address.zipcode}</p>
+                                    <p>{order.ship_address.country.name}</p>
+                                </Card.Section>
                             </Card>
                         </Layout.Section>
 
                     </Layout>
                 </div>
-                <div className="delete">
-
-                </div>
-
             </div>
         );
     }
